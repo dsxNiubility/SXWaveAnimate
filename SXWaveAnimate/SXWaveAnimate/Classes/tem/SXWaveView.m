2b607a7c82523509c20dfbfd49d16b0989190374
董尚先 <dantesx2012@gmail.com>
2015-07-23 15:14:12 +0800

    建立临时文件夹，将从cell出发实现SXWaveView

---
+++ SXWaveAnimate/SXWaveAnimate/Classes/tem/SXWaveView.m
@@ -0,0 +1,21 @@
+//
+//  SXWaveView.m
+//  SXWaveAnimate
+//
+//  Created by dongshangxian on 15/7/23.
+//  Copyright (c) 2015年 Sankuai. All rights reserved.
+//
+
+#import "SXWaveView.h"
+
+@implementation SXWaveView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
