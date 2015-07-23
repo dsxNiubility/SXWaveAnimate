48f831cc3381251404a2a8841ee03760aafa6e4c
董尚先 <dongshangxian@meituan.com>
2015-07-23 14:50:29 +0800

    建立尝试的文件夹tem。 准备将项目的粒度从cell转移至view

---
+++ SXWaveAnimate/SXWaveAnimate/Classes/tem/SXWaveView.h
@@ -0,0 +1,13 @@
+//
+//  SXWaveView.h
+//  SXWaveAnimate
+//
+//  Created by dongshangxian on 15/7/23.
+//  Copyright (c) 2015年 Sankuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SXWaveView : UIView
+
+@end
