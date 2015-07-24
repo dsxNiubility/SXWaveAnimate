643d489edeb90b908df778d43ca8e98ab51591af
董尚先 <dantesx2012@gmail.com>
2015-07-24 09:49:53 +0800

    建立与cell类似的属性列表并连线

--- SXWaveAnimate/SXWaveAnimate/Classes/tem/SXWaveView.h
+++ SXWaveAnimate/SXWaveAnimate/Classes/tem/SXWaveView.h
@@ -9,5 +9,23 @@
 #import <UIKit/UIKit.h>
 
 @interface SXWaveView : UIView
+@property (weak, nonatomic) IBOutlet UIView *leftView;
+@property (weak, nonatomic) IBOutlet UIImageView *rotateImg;
+@property (weak, nonatomic) IBOutlet UILabel *avgScoreLbl;
+@property (weak, nonatomic) IBOutlet UILabel *discriptionLbl;
+
+@property(nonatomic,strong)UIImageView *bigImg;
+
+@property(nonatomic,assign)CGFloat alpha;
+
+@property(nonatomic,strong)UIColor *textColor;
+
+@property(nonatomic,assign)int type;
+
+@property(nonatomic,assign,getter=isEndless)BOOL endless;
+
+- (instancetype)initWithPrecent:(int)precent;
+- (void)addAnimateWithType:(int)type;
+- (void)setPrecent:(int)precent textColor:(UIColor *)tcolor type:(int)type alpha:(CGFloat)alpha;
 
 @end
