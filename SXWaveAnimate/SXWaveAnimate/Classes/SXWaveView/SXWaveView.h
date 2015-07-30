94062a2220097bdb8b77ec74db1adfbbfe14e633
董尚先 <dantesx2012@gmail.com>
2015-07-30 17:46:47 +0800

    修改了一些细节

--- SXWaveAnimate/SXWaveAnimate/Classes/SXWaveView/SXWaveView.h
+++ SXWaveAnimate/SXWaveAnimate/Classes/SXWaveView/SXWaveView.h
@@ -10,21 +10,21 @@
 #import "UIViewAdditions.h"
 
 @interface SXWaveView : UIView
-@property (weak, nonatomic) IBOutlet UIView *leftView;
-@property (weak, nonatomic) IBOutlet UIImageView *rotateImg;
-@property (weak, nonatomic) IBOutlet UILabel *avgScoreLbl;
-@property (weak, nonatomic) IBOutlet UILabel *descriptionLbl;
-@property(nonatomic,assign)int precent;
+@property (weak, nonatomic) UIView *leftView;
+@property (weak, nonatomic) UIImageView *rotateImg;
+@property (weak, nonatomic) UILabel *avgScoreLbl;
+@property (weak, nonatomic) UILabel *descriptionLbl;
+@property (nonatomic,assign) int precent;
 
-@property(nonatomic,strong)UIImageView *bigImg;
+@property (nonatomic,strong) UIImageView *bigImg;
 
-@property(nonatomic,assign)CGFloat alpha;
-@property(nonatomic,strong)UIColor *textColor;
-@property(nonatomic,strong)UIColor *bgColor;
-@property(nonatomic,assign)int type;
-@property(nonatomic,assign,getter=isEndless)BOOL endless;
-@property(nonatomic,copy)NSString *descriptionTxt;
-@property(nonatomic,assign)BOOL half;
+@property (nonatomic,assign) CGFloat alpha;
+@property (nonatomic,strong) UIColor *textColor;
+@property (nonatomic,strong) UIColor *bgColor;
+@property (nonatomic,assign) int type;
+@property (nonatomic,assign,getter=isEndless) BOOL endless;
+@property (nonatomic,copy) NSString *descriptionTxt;
+@property (nonatomic,assign) BOOL half;
 
 
 - (instancetype)initWithPrecent:(int)precent;
